6cb701314d4ca45ed87187c64c25e1f4f2967057
ilolbot-admin <ilol.bot.nu@gmail.com>
2011-02-27 00:52:44 -0800

    Cleaner code, more error checking and other random improvements

--- scripts/sendChat.php
+++ scripts/sendChat.php
@@ -3,23 +3,33 @@ session_start();
 require('database.php');
 require('thumbs.php');
 
+//$_SESSION['user_name'] = "LOL";
+
+function fatalError($error)
+{
+	die('<script type="text/javascript">alert("Error: '. $error .'.");</script>');
+}
+
+
+
 if(isset($_SESSION['user_name']))
 {
-	$user_name = $_SESSION['user_name'];
-	$user_name = mysql_real_escape_string($user_name);
-	$chat_message = $_POST['chat_message'];
-	$user_ip = $_SERVER['REMOTE_ADDR'];
-	$time_sent = date("Y-m-d H:i:s");
-
-	$errorLOL = "";
+	$error = "";
 
 	$query = mysql_query("SELECT * FROM banned WHERE user_ip='$user_ip'");
 	if(mysql_fetch_assoc($query) > 0) //Check if user is banned
 		$ban = true;
 
-	$file_uploaded = $_FILES['file']['name'];
 
 	if($ban == false) {
+		$user_name = $_SESSION['user_name'];
+		$user_name = mysql_real_escape_string($user_name);
+		$chat_message = $_POST['chat_message'];
+		$user_ip = $_SERVER['REMOTE_ADDR'];
+		$time_sent = date("Y-m-d H:i:s");
+		
+		$file_uploaded = $_FILES['file']['name'];
+
 	
 		$i=0;
 		$spam = 0;
@@ -27,31 +37,29 @@ if(isset($_SESSION['user_name']))
 
 		$result = mysql_query("SELECT * FROM message WHERE user_ip='$user_ip'");
 
-		while($row = mysql_fetch_array($result)){
+		while($row = mysql_fetch_array($result)) {
 			$postinfo = $row['chat_message'];
 			array_push($post_array, $postinfo);
 		}
 
-		while(count($post_array)>$i){
-
+		while(count($post_array)>$i) {
 			$Post_old = $post_array[$i];
 			$Post_new = $post_array[$i+1];
 
 			similar_text($Post_new, $Post_old, $sim);
 
-
-			if($sim>75){ //iIf they are the same
+			if($sim>75) { //iIf they are the same
 				$spam+=1;
-				if($spam>3){
+				if($spam>3) {
 					mysql_query("INSERT INTO banned (user_ip) VALUES ('$user_ip')");
 					$i = count($post_array);
 				}
 			}
 			$i+=1;
 		}
-	
-	
-	
+
+
+
 		if(!((strlen($chat_message) > 1000) || (strlen($chat_message) == 0))) {
 			$chat_message = htmlentities($chat_message);
 			$chat_message = str_replace("\n", "<br>", $chat_message);
@@ -60,9 +68,8 @@ if(isset($_SESSION['user_name']))
 			$bb_replace = array('<\1>\2</\1>');
 			$chat_message = preg_replace($bb_tags, $bb_replace, $chat_message);*/
 			//preg_replace("/&gt;(.*?)<br>/i", '<span style="color:#002">&gt;$1</span>', $chat_message);
-			
+
 			$chat_message = mysql_real_escape_string($chat_message);
-			//createthumb("uploads/cat.jpg","uploads/cat.jpg",100,100);
 		}
 			$file_id ="file";
 			if($_FILES[$file_id]['name']) {
@@ -74,9 +81,7 @@ if(isset($_SESSION['user_name']))
 				$ext_arr = split("\.",basename($file_title));
 				$ext = strtolower($ext_arr[count($ext_arr)-1]); //Get the last extension
 
-				//Not really uniqe - but for all practical reasons, it is
-				//$uniqer = substr(md5(uniqid(rand(),1)),0,5);
-				$file_title = md5_file($_FILES['file']['tmp_name']) . "." . $ext;//Get Unique Name
+				$file_title = md5_file($_FILES['file']['tmp_name']) . "." . $ext;//Get unique name
 				$uploadfile = $uploaddir . $file_title;
 				
 				
@@ -84,28 +89,31 @@ if(isset($_SESSION['user_name']))
 				if($types) {
 					if(!file_exists("images/" . $file_title)) {
 						if(in_array($ext,$all_types)){
-							move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)or die('<script type="text/javascript">alert("LOL!");</script>');
-							//sleep(1);
+							move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)or fatalError("Could not move file");
+
 							createthumb("images/".$file_title,"thumbs/".$file_title,100,100);
-							//sleep(1);
-							mysql_query("INSERT INTO message (user_name, chat_message, image, user_ip, time_sent) VALUES ('$user_name', '$chat_message', '$file_title', '$user_ip', '$time_sent')") or die('<script type="text/javascript">alert("'. mysql_error() .' LOL!");</script>');
+
+							mysql_query("INSERT INTO message (user_name, chat_message, image, user_ip, time_sent) VALUES ('$user_name', '$chat_message', '$file_title', '$user_ip', '$time_sent')") or fatalError("Database error");
 						}
 						else
-							$errorLOL = 'CANNOT UPLOAD FILE TYPE';
+							$error = 'Cannot upload file type';
 					}
 					else
-						$errorLOL = 'FILE ALREADY EXISTS';
+						$error = 'File already exists';
 				}
 			}
 			else if($chat_message)
-				mysql_query("INSERT INTO message (user_name, chat_message, user_ip, time_sent) VALUES ('$user_name', '$chat_message', '$user_ip', '$time_sent')");
+				mysql_query("INSERT INTO message (user_name, chat_message, user_ip, time_sent) VALUES ('$user_name', '$chat_message', '$user_ip', '$time_sent')") or fatalError("Database error");
 			else
-				$errorLOL = 'YOU MUST POST SOMETHING';
+				$error = 'You must post something';
 
 	}
 	else
-		$errorLOL = "U BANNED";
-	if($errorLOL != "")
-		echo '<script type="text/javascript">alert("'. $errorLOL .' LOL!");</script>';
+		$error = "You have been muted.";
 }
+else
+	$error = "Your session has expired, please refresh the page";
+
+if($error != "")
+	fatalError($error);
 ?>
